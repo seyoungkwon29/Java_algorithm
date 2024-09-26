b0d99ddbb7693562b6aca1ac3a4070e608adb31b
seyoungkwon <seyoungkwon29@gmail.com>
2024-09-26 17:23:57 +0900

    첫번째 커밋

---
+++ src/test/java/com/java_algorithm/JavaAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
